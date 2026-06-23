98e01a637981c1a8cda5d4dcb8fd3ae7e8a0e8e9
Leonid Bloch <lb.workbox@gmail.com>
2026-06-23 11:35:33 +0300

    space-nullifier: Avoid modifying diff/patch files
    Signed-off-by: Leonid Bloch <lb.workbox@gmail.com>

--- space-nullifier/space-nullifier-with-json.sh
+++ space-nullifier/space-nullifier-with-json.sh
@@ -21,7 +21,7 @@ git_root="$(git rev-parse --show-toplevel)"
 find "${git_root}/" -path "${git_root}/.git" -prune -o -type f -print0 | xargs -0 sh -c '
     for file in "${@}"
     do
-        if file --mime "${file}" | grep -q "charset=[^[:space:]]*\(ascii\|utf-8\)"
+        if file --mime "${file}" | grep -q "charset=[^[:space:]]*\(ascii\|utf-8\)" | grep -v "text/[^[:space:]]*diff"
         then
             sed -i "s/[[:space:]]\+$//" "${file}"
             [ -z "$(tail -c1 "${file}")" ] || echo >> "${file}"
